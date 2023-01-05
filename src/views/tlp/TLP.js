53745a5b30a0a19928b84de39b6ea4996c278854
Patricio Bolino <patriciobolino@gmail.com>
2023-01-05 15:32:05 -0300

    tlp with backend data

--- src/views/tlp/TLP.js
+++ src/views/tlp/TLP.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import { Row, Col, Card, Table, Breadcrumb } from 'react-bootstrap';
+import { getTLP } from '../../api/services/tlp';
+import { useState , useEffect } from "react";
 
 const ListTLP = () => {
+
+    const [tlp, setTLP] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        getTLP()
+        .then((response) => {
+            setTLP(response.data.results);
+            setError(null);
+        })
+        .catch(setError);
+    }, []);
+    
+    if (error) {
+        console.log(error);
+        return <p>Ups! Se produjo un error al buscar el protocolo de semaforo.</p>
+    }
+
     return (
         <React.Fragment>
             <Row>
@@ -48,34 +68,15 @@ const ListTLP = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td><p class="p-3 mb-2 bg-dark text-white">TLP:WHITE</p></td>
-                                        <td>Disclosure is not limited.</td>
-                                        <td>Sources may use TLP:WHITE when information carries minimal or no foreseeable risk of misuse, in accordance with applicable rules and procedures for public release.</td>
-                                        <td>Subject to standard copyright rules, TLP:WHITE information may be distributed without restriction.</td>                  
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">2</th>
-                                        <td><p p class="p-3 mb-2 bg-success text-white">TLP:GREEN</p></td>
-                                        <td>Limited disclosure, restricted to the community.</td>
-                                        <td>Sources may use TLP:GREEN when information is useful for the awareness of all participating organizations as well as with peers within the broader community or sector.</td>
-                                        <td>Recipients may share TLP:GREEN information with peers and partner organizations within their sector or community, but not via publicly accessible channels. Information in this category can be circulated widely within a particular community. TLP:GREEN information may not be released outside of the community.</td>                             
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td><p class="p-3 mb-2 bg-warning text-dark">TLP:AMBER</p></td>
-                                        <td>Limited disclosure, restricted to participants organizations.</td>
-                                        <td>Sources may use TLP:AMBER when information requires support to be effectively acted upon, yet carries risks to privacy, reputation, or operations if shared outside of the organizations involved.</td>
-                                        <td>Recipients may only share TLP:AMBER information with members of their own organization, and with clients or customers who need to know the information to protect themselves or prevent further harm. Sources are at liberty to specify additional intended limits of the sharing: these must be adhered to.</td>                              
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">4</th>
-                                        <td><p class="p-3 mb-2 bg-danger text-white">TLP:RED</p></td>
-                                        <td>Not for disclosure, restricted to participants only.</td>
-                                        <td>Sources may use TLP:RED when information cannot be effectively acted upon by additional parties, and could lead to impacts on a party's privacy, reputation, or operations if misused.</td>
-                                        <td>Recipients may not share TLP:RED information with any parties outside of the specific exchange, meeting, or conversation in which it was originally disclosed. In the context of a meeting, for example, TLP:RED information is limited to those present at the meeting. In most circumstances, TLP:RED should be exchanged verbally or in person.</td>                              
-                                    </tr>
+                                    {tlp.map((item,i) => (
+                                        <tr>
+                                            <th scope="row">{i+1}</th>
+                                            <td><p style={{backgroundColor: item.color}}>{item.information}</p></td>
+                                            <td>{item.description}</td>
+                                            <td>{item.when}</td>
+                                            <td>{item.why}</td>
+                                        </tr>
+                                     ))}
                                 </tbody>
                             </Table>
                         </Card.Body>
