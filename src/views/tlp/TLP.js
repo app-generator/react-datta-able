69225fe59c703e71974c8a9d0e0e13740d26b8ee
Patricio Bolino <patriciobolino@gmail.com>
2023-01-12 14:55:11 -0300

    TLP search

--- src/views/tlp/TLP.js
+++ src/views/tlp/TLP.js
@@ -7,6 +7,10 @@ const ListTLP = () => {
 
     const [tlp, setTLP] = useState([]);
     const [error, setError] = useState(null);
+<<<<<<< Updated upstream
+=======
+    const [search, setSearch] = useState("");
+>>>>>>> Stashed changes
 
     useEffect(() => {
         getTLP()
@@ -22,6 +26,24 @@ const ListTLP = () => {
         return <p>Ups! Se produjo un error al buscar el protocolo de semaforo.</p>
     }
 
+<<<<<<< Updated upstream
+=======
+    //valores ingresados
+    const searcher = (e) => {
+        setSearch(e.target.value) //actualizar
+    }
+
+    //filtro
+    let list = []
+    if (!search) {
+        list = tlp
+    } else {
+        list = tlp.filter( (item) => 
+            item.name.toLowerCase().includes(search.toLocaleLowerCase())
+        )
+    }
+
+>>>>>>> Stashed changes
     return (
         <React.Fragment>
             <Row>
@@ -44,6 +66,7 @@ const ListTLP = () => {
                             <Row>
                                 <Col>
                                     <React.Fragment>
+<<<<<<< Updated upstream
                                         <div id="main-search" className='open'>
                                             <div className="input-group">
                                                 <input type="text" id="m-search" className="form-control" placeholder="Buscar protocolo de semaforo . . ." />
@@ -51,6 +74,13 @@ const ListTLP = () => {
                                                     <i className="fas fa-search " />
                                                 </span>
                                             </div>
+=======
+                                        <div className="input-group">
+                                            <input value={search} onChange={searcher} type="text" id="m-search" className="form-control" placeholder="Buscar protocolo de semaforo . . ." />
+                                            <span className="search-btn btn btn-primary">
+                                                <i className="feather icon-search " />
+                                            </span>
+>>>>>>> Stashed changes
                                         </div>
                                     </React.Fragment>                           
                                 </Col>                                  
@@ -68,8 +98,14 @@ const ListTLP = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
+<<<<<<< Updated upstream
                                     {tlp.sort((a,b) => a.code - b.code).map((item,i) => (
                                         <tr>
+=======
+                                    {list.sort((a,b) => a.code - b.code).map((item,i) => (
+                                        console.log("Hola"),
+                                        <tr key={i}>
+>>>>>>> Stashed changes
                                             <th scope="row">{i+1}</th>
                                             <td><p class="p-3 mb-2 bg-dark rounded" style={{color: item.color}}><b>{item.information}</b></p></td>
                                             <td><Form.Control style={{resize:"none"}} as="textarea" rows={3} plaintext readOnly defaultValue={item.description} /></td>
