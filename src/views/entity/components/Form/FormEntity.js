3014be630d322a66995971cb24934bf86c465076
veroyols <vero.scholz@gmail.com>
2023-02-16 16:34:10 -0300

    Validar espacio

--- src/views/entity/components/Form/FormEntity.js
+++ src/views/entity/components/Form/FormEntity.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import {Button, Form} from 'react-bootstrap';
-import { validateAlphanumeric } from '../../../user/validators' 
+import { validateAlphanumeric, validateSpace } from '../../../../components/Validator/validators'; 
 
 const FormEntity = (props) => { // props: name, setName, ifConfirm
 
     return (
         <React.Fragment>
             <Form>
-                <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Group controlId="Form.Entity.Name">
                     <Form.Label>Nombre</Form.Label>
                     <Form.Control 
-                        value={props.name} 
-                        onChange={(e) => props.setName(e.target.value)} 
-                        isInvalid={props.name === '' || !validateAlphanumeric(props.name)}
-                        isValid={props.name !== '' && validateAlphanumeric(props.name)}
                         type="nombre" 
                         placeholder="Nombre" 
-                        maxLength="255"/>
-                    {props.name!=='' ? '' : <div className="invalid-feedback">Ingrese nombre</div>}
+                        maxlength="100"
+                        value={props.name} 
+                        onChange={(e) => props.setName(e.target.value)} 
+                        isInvalid={!validateAlphanumeric(props.name) || !validateSpace(props.name)}
+                        isValid={validateAlphanumeric(props.name) && validateSpace(props.name)}
+                        />
+                    {validateSpace(props.name) ? '' : <div className="invalid-feedback">Ingrese nombre</div>}
                     {!props.name || validateAlphanumeric(props.name) ? "" : <div className="invalid-feedback">Ingrese caracteres validos</div>}
                 </Form.Group>
 
-                {props.name === '' || !validateAlphanumeric(props.name) ? 
+                {!validateSpace(props.name) || !validateAlphanumeric(props.name) ? 
                     <><Button variant="primary" disabled>Guardar</Button></> 
                     : 
                     <><Button variant="primary" onClick={props.ifConfirm}>Guardar</Button></>}
