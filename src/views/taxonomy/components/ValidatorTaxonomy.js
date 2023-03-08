82adcdd534804c86d220d76633b187d51a492a9d
Patricio Bolino <patriciobolino@gmail.com>
2023-03-08 12:04:48 -0300

    add validators for taxonomy

---
+++ src/views/taxonomy/components/ValidatorTaxonomy.js
@@ -0,0 +1,12 @@
+import { validateFieldText, validateLength, isEmpty } from "../../../components/Validator/validators"
+
+const validateName = (name) =>{
+    return (validateFieldText(name) && validateLength(name, 100) && !isEmpty(name))
+}
+
+
+const validateDescription = (description) =>{
+    return (!isEmpty(description))
+}
+
+export {validateName, validateDescription}
