e5e9e6e97edeca1bd69ef83c6ffede32220efc5d
fran1999 <c.javierfranco99@gmail.com>
2023-03-08 15:15:45 -0300

    se exportan validadores

--- src/components/Validator/validators.js
+++ src/components/Validator/validators.js
@@ -57,4 +57,4 @@ const validateCidr = (cidr) => {
   return  (/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\/(3[0-2]|2[0-9]|[01]?[0-9])$/).test(cidr);
 }
 
-export {validateEmail, validateFieldText, validateUsername, validateSpaces, validateNumber, validateAlphanumeric, validateNumbers, validateSpace, validateURL, validateCidr};
+export {validateEmail, validateFieldText, validateUsername, validateSpaces, validateNumber, validateAlphanumeric, validateNumbers, validateSpace, validateURL, validateCidr, validateLength, isEmpty};
