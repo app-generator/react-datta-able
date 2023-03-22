de22ce29ec37b1a96d99e5c7a7177563cde1c4ab
Patricio Bolino <patriciobolino@gmail.com>
2023-03-22 16:43:14 -0300

    add alert messages in service Taxonomy and TLP

--- src/api/services/tlp.js
+++ src/api/services/tlp.js
@@ -3,11 +3,12 @@ import { COMPONENT_URL } from '../../config/constant';
 import setAlert from '../../utils/setAlert';
 
 const getTLP = () => {
+    let messageError = `No se pudo recuperar la informacion de TLP`;
     return apiInstance.get(COMPONENT_URL.tlp)
     .then(response => {        
         return response;
     }).catch( error => { 
-        setAlert("No se pudo recuperar la informacion de TLP", "error");
+        setAlert(messageError, "error");
         return Promise.reject(error);
     });
 }
