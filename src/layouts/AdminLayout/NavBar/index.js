579380ef8ab9e51f7e5339c44df900a0c0ff5bb1
Mateo Durante <mateodurante@gmail.com>
2024-01-08 16:57:53 +0100

    NGEN at mobile navbar

--- src/layouts/AdminLayout/NavBar/index.js
+++ src/layouts/AdminLayout/NavBar/index.js
@@ -35,7 +35,7 @@ const NavBar = () => {
                     <div className="b-bg">
                         <i className="feather icon-trending-up" />
                     </div>
-                    <span className="b-title">Datta Able</span>
+                    <span className="b-title">NGEN</span>
                 </Link>
                 {/* <Link to='#' className={moreClass.join(' ')} onClick={() => setMoreToggle(!moreToggle)}>
                     <i className="feather icon-more-vertical"/>
